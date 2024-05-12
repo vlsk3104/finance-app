7d9603feafea5c2fde6a755bb365eeec6acd3475
vlsk3104 <vlsk3104@gmail.com>
2024-05-12 20:53:32 +0900

    Accounts POST API

---
+++ features/accounts/api/use-create-account.ts
@@ -0,0 +1,26 @@
+import { client } from '@/lib/hono';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { InferRequestType, InferResponseType } from 'hono';
+import { toast } from 'sonner';
+
+type ResponseType = InferResponseType<typeof client.api.accounts.$post>;
+type RequestType = InferRequestType<typeof client.api.accounts.$post>['json'];
+
+export const useCreateAccount = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.accounts.$post({ json });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success('アカウントを作成しました');
+      queryClient.invalidateQueries({ queryKey: ['accounts'] });
+    },
+    onError: () => {
+      toast.error('アカウントの作成に失敗しました');
+    },
+  });
+
+  return mutation;
+};
---
+++ features/accounts/components/account-form.tsx
@@ -0,0 +1,91 @@
+import { insertAccountSchema } from '@/db/schema';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Trash } from 'lucide-react';
+
+const formSchema = insertAccountSchema.pick({ name: true });
+
+type FormValues = z.input<typeof formSchema>;
+
+type Props = {
+  id?: string;
+  defaultValues?: FormValues;
+  onSubmit: (values: FormValues) => void;
+  onDelete?: () => void;
+  disabled?: boolean;
+};
+
+const AccountForm = ({
+  id,
+  defaultValues,
+  onSubmit,
+  onDelete,
+  disabled,
+}: Props) => {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues,
+  });
+
+  const handleSubmit = (values: FormValues) => {
+    onSubmit(values);
+  };
+
+  const handleDelete = () => {
+    onDelete?.();
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="space-y-4 pt-4"
+      >
+        <FormField
+          name="name"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>名前</FormLabel>
+              <FormControl>
+                <Input
+                  disabled={disabled}
+                  placeholder="例：現金、銀行、クレジットカード"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" disabled={disabled}>
+          {id ? '保存する' : '作成する'}
+        </Button>
+        {!!id && (
+          <Button
+            type="button"
+            disabled={disabled}
+            onClick={handleDelete}
+            className="w-full"
+            variant="outline"
+          >
+            <Trash className="size-4 mr-4" />
+            アカウントを削除する
+          </Button>
+        )}
+      </form>
+    </Form>
+  );
+};
+
+export default AccountForm;
