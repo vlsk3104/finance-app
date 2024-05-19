6f51a4029dc4acfbbb5fd3d2602de81741f88f06
vlsk3104 <vlsk3104@gmail.com>
2024-05-19 17:12:34 +0900

    Dashboard Chart

---
+++ components/chart.tsx
@@ -0,0 +1,81 @@
+'use client';
+import React, { useState } from 'react';
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
+import { AreaChart, BarChart3, FileSearch, LineChart } from 'lucide-react';
+import AreaVariant from './area-variant';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select';
+import LineVariant from './line-variant';
+import BarVariant from './bar-variant';
+
+type Props = {
+  data?: {
+    date: string;
+    income: number;
+    expenses: number;
+  }[];
+};
+
+const Chart = ({ data = [] }: Props) => {
+  const [chartType, setChartType] = useState('area');
+
+  const onTypeChange = (type: string) => {
+    setChartType(type);
+  };
+
+  return (
+    <Card className="border-none drop-shadow-sm">
+      <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between">
+        <CardTitle className="text-xl line-clamp-1">取引</CardTitle>
+        <Select defaultValue={chartType} onValueChange={onTypeChange}>
+          <SelectTrigger className="lg:w-auto h-9 rounded-md px-3">
+            <SelectValue placeholder="チャート種類" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="area">
+              <div className="flex items-center">
+                <AreaChart className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">エリア</p>
+              </div>
+            </SelectItem>
+            <SelectItem value="line">
+              <div className="flex items-center">
+                <LineChart className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">ライン</p>
+              </div>
+            </SelectItem>
+            <SelectItem value="bar">
+              <div className="flex items-center">
+                <BarChart3 className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">バー</p>
+              </div>
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </CardHeader>
+      <CardContent>
+        {data.length === 0 ? (
+          <div className="flex flex-col gap-y-4 items-center justify-center h-[350px] w-full">
+            <FileSearch className="size-6 text-muted-foreground" />
+            <p className="text-muted-foreground text-sm">
+              当期のデータがありません
+            </p>
+          </div>
+        ) : (
+          <>
+            {chartType === 'line' && <LineVariant data={data} />}
+            {chartType === 'area' && <AreaVariant data={data} />}
+            {chartType === 'bar' && <BarVariant data={data} />}
+          </>
+        )}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Chart;
